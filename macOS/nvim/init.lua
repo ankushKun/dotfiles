f8f42792047fc72125432052bb12b71a7daeffc4
ankushKun <ankush4singh@gmail.com>
2022-06-14 08:30:00 +0530

    update dotfiles - 2022-06-14 08:30:00

--- macOS/nvim/init.lua
+++ macOS/nvim/init.lua
@@ -184,9 +184,12 @@ lspconfig.sumneko_lua.setup {
         }
     }
 }
-lspconfig.pyright.setup { capabilities = capabilities }
+lspconfig.pylsp.setup {
+    capabilities = capabilities
+}
 lspconfig.omnisharp.setup {
-    cmd = { "mono", "/Users/ankush/.local/share/nvim/lsp_servers/omnisharp/omnisharp-mono/OmniSharp.exe", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+    cmd = { "mono", "/Users/ankush/.local/share/nvim/lsp_servers/omnisharp/omnisharp-mono/OmniSharp.exe",
+        "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
     root_dir = lspconfig.util.root_pattern("*.sln");
     capabilities = capabilities,
     use_mono = true
