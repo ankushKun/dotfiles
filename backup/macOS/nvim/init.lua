c9c2fb5c798b6c650c885fab69e76e00a29c3a02
ankushKun <ankush4singh@gmail.com>
2022-06-18 13:45:00 +0530

    update dotfiles - 2022-06-18 13:45:00

--- backup/macOS/nvim/init.lua
+++ backup/macOS/nvim/init.lua
@@ -324,9 +324,17 @@ require('telescope').setup {
 --------------------------------------------------------------------
 local alpha = require('alpha')
 local dashboard = require('alpha.themes.dashboard')
-dashboard.section.header.val = {[[OK]]}
-dashboard.sections.buttons.val = {
-    dashboard.button("","Quit",":q")
+dashboard.section.header.val = {
+    [[                                __                ]],
+    [[   ___     ___    ___   __  __ /\_\    ___ ___    ]],
+    [[  / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+    [[  /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+    [[  \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+    [[  \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+}
+dashboard.section.buttons.val = {
+    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
 }
 dashboard.section.footer.val = "Wasted 100 hours to save 1 hour - Vim Philosophy"
 alpha.setup(dashboard.config)
