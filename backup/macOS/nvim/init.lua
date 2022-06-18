47a72f034ce2163ed82bb0d4ab538a7b795f5d0d
ankushKun <ankush4singh@gmail.com>
2022-06-18 14:00:00 +0530

    update dotfiles - 2022-06-18 14:00:00

--- backup/macOS/nvim/init.lua
+++ backup/macOS/nvim/init.lua
@@ -322,23 +322,54 @@ require('telescope').setup {
 --------------------------------------------------------------------
 --                           ALPHA                                --
 --------------------------------------------------------------------
-local alpha = require('alpha')
-local dashboard = require('alpha.themes.dashboard')
-dashboard.section.header.val = {
-    [[                                __                ]],
-    [[   ___     ___    ___   __  __ /\_\    ___ ___    ]],
-    [[  / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-    [[  /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-    [[  \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-    [[  \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
-}
-dashboard.section.buttons.val = {
-    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-    dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
-}
-dashboard.section.footer.val = "Wasted 100 hours to save 1 hour - Vim Philosophy"
-alpha.setup(dashboard.config)
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
 
+-- Set header
+dashboard.section.header.val = {
+    "                                                     ",
+    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                     ",
+}
+
+-- Set menu
+dashboard.section.buttons.val = {
+    dashboard.button( "e", " New File" , ":ene <BAR> startinsert <CR>"),
+    dashboard.button( "f", " Find File", ":Telescope find_files<CR>"),
+    dashboard.button( "l", " Find Word", ":Telescope live_grep<CR>"),
+    dashboard.button( "r", " Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "s", " Settings" , ":Config<CR>"),
+    dashboard.button( "q", " Quit NVIM", ":qa<CR>"),
+}
+
+-- Set footer
+--   NOTE: This is currently a feature in my fork of alpha-nvim (opened PR #21, will update snippet if added to main)
+--   To see test this yourself, add the function as a dependecy in packer and uncomment the footer lines
+--   ```init.lua
+--   return require('packer').startup(function()
+--       use 'wbthomason/packer.nvim'
+--       use {
+--           'goolord/alpha-nvim', branch = 'feature/startify-fortune',
+--           requires = {'BlakeJC94/alpha-nvim-fortune'},
+--           config = function() require("config.alpha") end
+--       }
+--   end)
+--   ```
+-- local fortune = require("alpha.fortune") 
+-- dashboard.section.footer.val = fortune()
+
+-- Send config to alpha
+alpha.setup(dashboard.opts)
+
+-- Disable folding on alpha buffer
+vim.cmd([[
+    autocmd FileType alpha setlocal nofoldenable
+]])
 --------------------------------------------------------------------
 --                      MARKDOWN PREVIEW                          --
 --------------------------------------------------------------------
