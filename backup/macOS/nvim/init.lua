811ab2c7a7c846722385883ebec166644aaafcbc
ankushKun <ankush4singh@gmail.com>
2022-06-19 11:00:00 +0530

    update dotfiles - 2022-06-19 11:00:00

--- backup/macOS/nvim/init.lua
+++ backup/macOS/nvim/init.lua
@@ -28,11 +28,11 @@ vim.opt.smarttab = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.guifont = "MesloLGS NF:h15"
 
-vim.opt.fillchars = [[eob: ,fold: ,foldopen:v,foldsep: ,foldclose:>]]
+-- vim.opt.fillchars = [[eob: ,fold: ,foldopen:v,foldsep: ,foldclose:>]]
 
 -- Neovide options
 if (vim.fn.exists('neovide') == 1) then
-    vim.g.neovide_transparency = 0.85
+    vim.g.neovide_transparency = 1
     vim.g.neovide_input_use_logo = 1
 end
 
@@ -206,6 +206,63 @@ nls.setup {
         nls.builtins.formatting.black
     }
 }
+
+--------------------------------------------------------------------
+--                        CODE FOLDING                            --
+--------------------------------------------------------------------
+vim.cmd([[
+set nofoldenable
+set foldlevel=99
+set fillchars=fold:\
+set foldtext=CustomFoldText()
+setlocal foldmethod=expr
+setlocal foldexpr=GetPotionFold(v:lnum)
+function! GetPotionFold(lnum)
+  if getline(a:lnum) =~? '\v^\s*$'
+    return '-1'
+  endif
+  let this_indent = IndentLevel(a:lnum)
+  let next_indent = IndentLevel(NextNonBlankLine(a:lnum))
+  if next_indent == this_indent
+    return this_indent
+  elseif next_indent < this_indent
+    return this_indent
+  elseif next_indent > this_indent
+    return '>' . next_indent
+  endif
+endfunction
+function! IndentLevel(lnum)
+    return indent(a:lnum) / &shiftwidth
+endfunction
+function! NextNonBlankLine(lnum)
+  let numlines = line('$')
+  let current = a:lnum + 1
+  while current <= numlines
+      if getline(current) =~? '\v\S'
+          return current
+      endif
+      let current += 1
+  endwhile
+  return -2
+endfunction
+function! CustomFoldText()
+  " get first non-blank line
+  let fs = v:foldstart
+  while getline(fs) =~ '^\s*$' | let fs = nextnonblank(fs + 1)
+  endwhile
+  if fs > v:foldend
+      let line = getline(v:foldstart)
+  else
+      let line = substitute(getline(fs), '\t', repeat(' ', &tabstop), 'g')
+  endif
+  let w = winwidth(0) - &foldcolumn - (&number ? 8 : 0)
+  let foldSize = 1 + v:foldend - v:foldstart
+  let foldSizeStr = " " . foldSize . " lines "
+  let foldLevelStr = repeat("+--", v:foldlevel)
+  let expansionString = repeat(" ", w - strwidth(foldSizeStr.line.foldLevelStr))
+  return line . expansionString . foldSizeStr . foldLevelStr
+endfunction
+]])
 
 --------------------------------------------------------------------
 --                          AUTOPAIRS                             --
