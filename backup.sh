764f6fae9e60676cd247e281be6ff0fc55f313ac
Ankush Singh <ankush4singh@gmail.com>
2022-06-27 07:58:22 +0530

    update dotfiles - 2022-06-27 07:58:22

--- backup.sh
+++ backup.sh
@@ -6,19 +6,19 @@ echo " "
 echo "---------------"
 echo "update dotfiles - `date +'%Y-%m-%d %H:%M:%S'`"
 
-/bin/cd $dotfiles
+/bin/cd "$dotfiles"
 
-/bin/rm -rf $dotfiles/backup/macOS
-/bin/mv $dotfiles/macOS $dotfiles/backup/
+/bin/rm -rf "$dotfiles/backup/macOS"
+/bin/mv "$dotfiles/macOS" "$dotfiles/backup"
 
-/bin/rm -rf $dotfiles/macOS
-/bin/mkdir $dotfiles/macOS
+/bin/rm -rf "$dotfiles/macOS"
+/bin/mkdir "$dotfiles/macOS"
 
-/bin/cp -r ~/.config/nvim/init.lua $dotfiles/macOS/init.lua
-/bin/cp -r ~/.config/alacritty/acacritty.yml $dotfiles/macOS/alacritty.yml
-/bin/cp -r ~/.zshrc $dotfiles/macOS/zshrc
-/bin/cp -r ~/.tmux.conf $dotfiles/macOS/tmux.conf
-/bin/cp -r ~/.p10k.zsh $dotfiles/macOS/p10k.zsh
+/bin/cp -r "~/.config/nvim/init.lua" "$dotfiles/macOS/init.lua"
+/bin/cp -r "~/.config/alacritty/acacritty.yml" "$dotfiles/macOS/alacritty.yml"
+/bin/cp -r "~/.zshrc" "$dotfiles/macOS/zshrc"
+/bin/cp -r "~/.tmux.conf" "$dotfiles/macOS/tmux.conf"
+/bin/cp -r "~/.p10k.zsh" "$dotfiles/macOS/p10k.zsh"
 
 git add .
 git commit -m "update dotfiles - `date +'%Y-%m-%d %H:%M:%S'`"
