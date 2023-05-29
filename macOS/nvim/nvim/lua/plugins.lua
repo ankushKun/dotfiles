5904ee001015b998cd362b6e162d73d6d548b2f0
ankushKun <ankush4singh@gmail.com>
2023-05-29 19:15:01 +0530

    update dotfiles - 2023-05-29 19:15:01

--- macOS/nvim/nvim/lua/plugins.lua
+++ macOS/nvim/nvim/lua/plugins.lua
@@ -99,4 +99,32 @@ require('packer').startup(function()
     use 'othree/html5.vim'
     use 'pangloss/vim-javascript'
     use 'evanleck/vim-svelte'
+
+    -- php laravel
+    use {
+        "adalessa/laravel.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "tpope/vim-dotenv",
+            "MunifTanjim/nui.nvim",
+        },
+        cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+        keys = {
+            { "<leader>la", ":Laravel artisan<cr>" },
+            { "<leader>lr", ":Laravel routes<cr>" },
+            {
+                "<leader>lt",
+                function()
+                    require("laravel.tinker").send_to_tinker()
+                end,
+                mode = "v",
+                desc = "Laravel Application Routes",
+            },
+        },
+        event = { "VeryLazy" },
+        config = function()
+            require("laravel").setup()
+            require("telescope").load_extension "laravel"
+        end,
+    }
 end)
